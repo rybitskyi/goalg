d95bc93b5a93cf603ff7e1df20e7df8756be18b9
Iurii  Rybitskyi <yury.ribitsky@gmail.com>
2018-05-01 17:56:56 -0700

    optimized getMaxProfit function.

--- interviewcake/stock-price/stockprice.go
+++ interviewcake/stock-price/stockprice.go
@@ -22,7 +22,23 @@ func getBruteForceMaxProfit(prices []int) int {
 	return max
 }
 
+func getMaxProfit(prices []int) int {
+	//TODO: throw exception in case arr length is less than 2
+	minPrice := prices[0]
+	max := prices[1] - prices[0]
+	for i := 1; i < len(prices); i++ {
+		profit := prices[i] - minPrice
+		if profit > max {
+			max = profit
+		}
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		}
+	}
+	return max
+}
+
 func main() {
 	stockPrices := []int{10, 7, 5, 8, 11, 9}
-	fmt.Println(fmt.Sprintf("max stock profit: %d", getBruteForceMaxProfit(stockPrices)))
+	fmt.Println(fmt.Sprintf("max stock profit: %d", getMaxProfit(stockPrices)))
 }
